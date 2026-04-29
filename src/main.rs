299642aed0d9f6c8ef9443d921c7d2615ebd6d29
Mathieu Borderé <mathieu@letmetweakit.com>
2026-04-29 16:12:31 +0200

    Initial commit.

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use rustc_hash::FxHashMap;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn main() {
+    let f = File::open("fileseq.txt").expect("open");
+    let reader = BufReader::new(f);
+    let mut m: FxHashMap<u64, ()> = FxHashMap::default();
+    let (mut ins_count, mut rm_count) = (0u64, 0u64);
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let parts: Vec<&str> = line.split_whitespace().collect();
+        if parts.len() != 3 || parts[0] != "DSTDIAG_FILESEQ" {
+            continue;
+        }
+        let id: u64 = parts[2].parse().unwrap();
+        match parts[1] {
+            "INS" => {
+                m.insert(id, ());
+                ins_count += 1;
+            }
+            "RM" => {
+                m.remove(&id);
+                rm_count += 1;
+            }
+            _ => {}
+        }
+    }
+    let order: Vec<u64> = m.keys().copied().collect();
+    println!(
+        "ins={} rm={} final_len={} cap={}",
+        ins_count,
+        rm_count,
+        m.len(),
+        m.capacity()
+    );
+    println!("iter_order: {:?}", order);
+}
